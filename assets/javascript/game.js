24f18ac26841ed78faadf8b4ee44538aa91728e3
Ajay Phombo <38087169+ajayphombo@users.noreply.github.com>
2019-02-27 21:47:27 -0800

    Add files via upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,42 +2,38 @@
         let win= 0;
         let loss= 0;
         let guessLeft= 10;
-        
+        var inputLetter=[];
         let letters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
-        "p","q","r","s","t","u","v","w","x","y","z"]// i wonder if there is a is simpler way to write this!!!
+        "p","q","r","s","t","u","v","w","x","y","z"]// 
         
-        var userInput= document.getElementById("user-text");//use value of
+        
 
         document.onkeyup= function(event){
-            userInput=event.key;
+           let userInput=event.key;//doesn't account for upper case letter
             console.log(event);
 
-            /*for(var i=0; i<10; i++){
-                guess
-            }*/
-
-           
-
         var randomLetter=letters[Math.floor(Math.random()*letters.length)];
-        console.log(randomLetter);
+        console.log(randomLetter);//i had trouble with this being here because every keyup event will generate a random.
+        //so there is a new random even if the firts random has not been guessed.
 
         if(userInput=== randomLetter){
             win++;
-            guessLeft=10;
+            guessLeft=10;//reset
+            inputLetter=[];//rest
         }
         
        
         else if(userInput!==randomLetter){
             guessLeft--;
+            inputLetter.push(userInput);
+            console.log(inputLetter);
             if(guessLeft===0){
                 loss++;
                 guessLeft=10;
+                inputLetter=[];//reset
             }
-        }
+        }//looks messy probably use some sort of functions!
     
-        
-        
-
         
 
         var winText=document.getElementById("win-text");
@@ -49,5 +45,9 @@
 
         var guess= document.getElementById("guess-number");
         guess.textContent= guessLeft;
+
+        var userInputText= document.getElementById("user-text");//use value of
+        userInputText.textContent= inputLetter;
         
+
     };
